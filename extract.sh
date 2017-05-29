0ba10027e432253f2a8fee912c9bf859453b80eb
umardx <umardx@s.itb.ac.id>
2017-05-29 16:37:31 +0700

    nope

--- extract.sh
+++ extract.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-directory=$1/ # pick all file in this directory
+directory=$( pwd )"/" # pick all file in this directory
 sDirectory=$directory*
 wFile=$directory"wifiList"
 
@@ -14,7 +14,7 @@ for file in $sDirectory
 do
  echo ${file/$directory/}| tr '\n' ','s >> ./$wFile
  less $file | grep Address | awk '{print $5}' | tr '\n' ','s >> ./$wFile
-# less $file | grep Signal | awk '{print $3}'| cut -b 7-9 | tr '\n' ','s >> ./$wFile
-# less $file | grep ESSID | sed 's/                    ESSID:"//' | sed 's/"//' | tr '\n' ','s >> ./$wFile
+ less $file | grep Signal | awk '{print $3}'| cut -b 7-9 | tr '\n' ','s >> ./$wFile
+ less $file | grep ESSID | sed 's/                    ESSID:"//' | sed 's/"//' | tr '\n' ','s >> ./$wFile
  echo '' >> ./$wFile
 done
--- scan.sh
+++ scan.sh
@@ -8,6 +8,6 @@ while(( $diskUse < 80 )) # limit disk use under 30%
 do
   diskUse=$(df -H | grep root | awk '{ print $5}' | cut -d'%' -f1)
   echo "Disk Use" $diskUse "%"
-  #./aplist.sh
-  #sleep 5    # for command and control
+  ./aplist.sh
+  sleep 5    # for command and control
 done
