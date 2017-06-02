c151e873ab1283fa02cfba12b9cf53657a7696cb
umardx <umardx@s.itb.ac.id>
2017-06-02 21:25:47 +0700

    new file:   iwlist.sh 	modified:   wiscan.py

---
+++ iwlist.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+## print header lines
+echo ""
+echo " mac                 essid         frq   chn qual   lvl  enc"
+
+while IFS= read -r line; do
+
+    ## test line contenst and parse as required
+    [[ "$line" =~ Address ]] && mac=${line##*ss: }
+    [[ "$line" =~ \(Channel ]] && { chn=${line##*nel }; chn=${chn:0:$((${#chn}-1))}; }
+    [[ "$line" =~ Frequen ]] && { frq=${line##*ncy:}; frq=${frq%% *}; }
+    [[ "$line" =~ Quality ]] && { 
+        qual=${line##*ity=}
+        qual=${qual%% *}
+        lvl=${line##*evel=}
+        lvl=${lvl%% *}
+    }
+    [[ "$line" =~ Encrypt ]] && enc=${line##*key:}
+    [[ "$line" =~ ESSID ]] && {
+        essid=${line##*ID:}
+        echo " $mac  $essid  $frq  $chn  $qual  $lvl  $enc"  # output after ESSID
+    }
+
+done
--- wiscan.py
+++ wiscan.py
@@ -1,30 +1,54 @@
 #!/usr/bin/env python
-import firebase
+from elasticsearch import Elasticsearch
 import datetime
+import socket
+import fcntl
+import struct
 import json
+import time
 import sys
 import re
 
-url = 'https://wifiscan-8076f.firebaseio.com'
-ts = datetime.datetime.now()
+def get_ip_address(ifname):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    return socket.inet_ntoa(fcntl.ioctl(
+        s.fileno(),
+        0x8915,  # SIOCGIFADDR
+        struct.pack('256s', ifname[:15])
+    )[20:24])
 
 chan_list = []
 freq_list = []
 sign_list = []
 ssid_list = []
 maca_list = []
+qual_list = []
+encr_list = []
+mode_list = []
+beac_list = []
 
-chan_re = re.compile(r'Channel:([0-9]+)')
-freq_re = re.compile(r'Frequency:([0-9.]+)')
-sign_re = re.compile(r'Signal level=([0-9-]+)')
-ssid_re = re.compile(r'ESSID:"(.*?)"')
-maca_re = re.compile(r'Address: ([0-9A-Fa-f]+:[0-9A-Fa-f]+:[0-9A-Fa-f]+:[0-9A-Fa-f]+:[0-9A-Fa-f]+:[0-9A-Fa-f]+)')
+chel_re = re.compile(r'Cell ([0-9]{2})')                    # Cell number
+
+chan_re = re.compile(r'Channel:([0-9]{1,2})')               # Channel
+freq_re = re.compile(r'Frequency:([0-9.]{5})')              # Frequency (GHz)
+sign_re = re.compile(r'Signal level=([0-9-]{3})')           # Signal Level (dBm)
+qual_re = re.compile(r'Quality=([0-9]{2})')                 # Quality (/70)
+ssid_re = re.compile(r'ESSID:\"(.*?)\"')                    # SSID
+encr_re = re.compile(r'Encryption key:([a-z]{2,3})')        # Encryption (on/off)
+mode_re = re.compile(r'Mode:([a-zA-Z-]{1,20})')             # Mode (Master, Ad-Hoc, etc)
+maca_re = re.compile(r'Address: ((?:[0-9a-fA-F]:?){12})')   # MAC Address
+beac_re = re.compile(r'Last beacon: ([0-9]{1,5})')          # Last Beacon (ms ago)
 
 for line in sys.stdin:
     chan_match = chan_re.match(line)
     chan = chan_re.search(line)
     if ( chan ):
         chan_list.append(int(chan.group(1)))
+
+    qual_match = qual_re.match(line)
+    qual = qual_re.search(line)
+    if ( qual ):
+        qual_list.append(int(qual.group(1)))
 
     freq_match = freq_re.match(line)
     freq = freq_re.search(line)
@@ -41,23 +65,48 @@ for line in sys.stdin:
     if ( ssid ):
         ssid_list.append(ssid.group(1))
 
+    encr_match = encr_re.match(line)
+    encr = encr_re.search(line)
+    if ( encr ):
+        encr_list.append(encr.group(1))
+
+    mode_match = mode_re.match(line)
+    mode = mode_re.search(line)
+    if ( mode ):
+        mode_list.append(mode.group(1))
+
     maca_match = maca_re.match(line)
     maca = maca_re.search(line)
     if ( maca ):
         maca_list.append(maca.group(1))
 
-data = {"timestamp":ts}
+    beac_match = beac_re.match(line)
+    beac = beac_re.search(line)
+    if ( beac ):
+        beac_list.append(int(beac.group(1)))
+
+index = "wifiscan-" + time.strftime("%Y.%m.%d")
+ts = datetime.datetime.now()
+ip = get_ip_address("wlan0")
+hn = socket.gethostname()
+
+data = {
+'Timestamp': ts,
+'Hostname': hn,
+'Address': ip,
+}
+
+es = Elasticsearch(timeout=480)
 for i in range(0,(len(ssid_list))):
     data.update({
-        i:{
         'SSID':ssid_list[i],
-        'MAC':maca_list[i],
+        'Encryption':encr_list[i],
+        'MAC Address':maca_list[i],
         'Channel':chan_list[i],
-        'Frequency':freq_list[i],
-        'Signal':sign_list[i]
-        }
+        'Mode':mode_list[i],
+        'Frequency (GHz)':freq_list[i],
+        'Signal (dBm)':sign_list[i],
+        'Quality (70)':qual_list[i],
+        'Last Beacon (ms ago)':beac_list[i]
     })
-
-
-firebase = firebase.FirebaseApplication(url, None)
-res = firebase.post("/APlist", data)
+    res = es.index(index=index, doc_type='scanlog', body=data)
