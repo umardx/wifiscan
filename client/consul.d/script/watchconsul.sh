d60a3cda372301323e8153f867b8e3ba40836a6d
umardx <umardx@s.itb.ac.id>
2017-07-05 13:50:50 +0700

    modified:   client/consul.d/client/config.json 	modified:   client/consul.d/client/wifiscan.json 	modified:   client/consul.d/script/watchconsul.sh 	modified:   client/setup.sh 	modified:   client/systemd/consul.service 	new file:   client/wpa_supplicant.conf

--- client/consul.d/script/watchconsul.sh
+++ client/consul.d/script/watchconsul.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
+dev="wlan0"
 
 echo $$ > /var/run/watchconsul.pid
 
 [ -f /var/consul/oldip.tmp ] || touch /var/consul/oldip.tmp
 
 consul_setup() {
-
   newip=$(/sbin/ifconfig wlan0 | grep "inet addr" | cut -d ':' -f 2 | cut -d ' ' -f 1)
   oldip=$(/bin/cat /var/consul/oldip.tmp)
 
   if [ "$oldip" != "$newip" ] && ( /bin/systemctl status consul > /dev/null 2>&1 ) && [ "$newip" != "" ]; then
-     echo "GANTI $oldip ke $newip" >> /root/gantiip.log
+     #echo "GANTI $oldip ke $newip" >> /root/gantiip.log
      /usr/local/bin/consul leave > /dev/null 2>&1 &&
      /bin/systemctl restart consul.service > /dev/null 2>&1 &&
      echo "$newip" > /var/consul/oldip.tmp
      echo "$oldip -> $newip : Consul Restarted."
+  fi
+}
+
+wifi_setup() {
+  if (/sbin/ifconfig $dev | grep "inet addr" >/dev/null 2>&1) && (ping -c1 8.8.8.8 >/dev/null 2>&1); then
+    sleep 1
+  else
+    /bin/systemctl restart networking && /sbin/dhclient $dev && sleep 15
   fi
 }
 
@@ -28,5 +36,5 @@ setup_wait() {
 
 while [ true ]; do
   setup_wait
-  consul_setup && sleep 1
+  wifi_setup && consul_setup && sleep 1
 done
--- client/setup.sh
+++ client/setup.sh
@@ -1,23 +1,51 @@
 #!/bin/bash
+node="192.168.114.30"
+WORKDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+#echo $WORKDIR
 setHostname() {
 	oldhost=$(cat /etc/hostname)
 	echo "Existing hostname is $oldhost"
 
-	urandom="$(cat /dev/urandom | tr -dc A-Z0-9|head -c5 | tr '[:upper:]' '[:lower:]')"
-	newhost="$(ifconfig | grep wlan0 | awk '{print $NF}' | sed 's/://g' | tr '[:upper:]' '[:lower:]')$urandom"
+	cpuid="$(cat /proc/cpuinfo | grep Serial | cut -d ' ' -f 2 | tail -c9)"
+	newhost="${cpuid}$(ifconfig | grep wlan0 | awk '{print $NF}' | sed 's/://g' | tr '[:upper:]' '[:lower:]')"
 
 	# change hostname in /etc/hosts & /etc/hostname
 	sed -i "s/$oldhost/$newhost/g" /etc/hosts
 	sed -i "s/$oldhost/$newhost/g" /etc/hostname
-  hostname -F /etc/hostname
+	hostname -F /etc/hostname
 	echo "New hostname set to $newhost"
 }
 
+setConsul() {
+	# Install consul arm64
+	apt-get update
+	apt-get install -y wget unzip
+	wget -O consul.zip "https://releases.hashicorp.com/consul/0.8.5/consul_0.8.5_linux_arm64.zip"
+	unzip -qq consul.zip -d /usr/local/bin/
+	rm consul.zip
+	mkdir -p /etc/consul.d/{client,script}
+	mkdir /var/consul
+
+	# Add host
+	echo "# Consul node address" >> /etc/hosts
+	echo "$node1 node1 kibana1.dx consul1.dx elastic1.dx" >> /etc/hosts
+
+	# Add systemd
+	cp $WORKDIR/systemd/consul.service /etc/systemd/system/
+	cp $WORKDIR/systemd/watchconsul.service /etc/systemd/system/
+	systemctl daemon-reload
+	systemctl enable consul.service
+	systemctl start consul.service
+	systemctl enable watchconsul.service
+	systemctl start watchconsul.service
+}
+
+# MAIN
 # Set hostname
 if [[ $EUID -ne 0 ]]; then
-	echo "You must be a root user" 2>&1
+	echo "You must be a root" 2>&1
 	exit 1
 else
-  setHostname
+	setHostname
+	setConsul
 fi
-
